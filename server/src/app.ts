9f435f56a79958313dde07762cf4a4813bced000
kimn <a9523023650@gmail.com>
2024-01-14 01:51:29 +0800

    [新增] cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,18 @@
-import "reflect-metadata";
-import express from "express";
 import cookieSession from "cookie-session";
-import "@/controller/login";
+import cors from "cors";
+import express from "express";
+import "reflect-metadata";
+
 import "@/controller/crawler";
 import "@/controller/health";
-import router from "./router";
+import "@/controller/login";
+import router from "@/router";
 
 const app = express();
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(
   cookieSession({
     name: "session",
